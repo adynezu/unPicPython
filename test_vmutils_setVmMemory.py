e4f4fba9f6ee6cbeae2753110f325399b077cb52
Adrian Suhov <adynezu@gmail.com>
2014-07-22 17:53:15 +0300

    Update test_vmutils_setVmMemory.py

--- test_vmutils_setVmMemory.py
+++ test_vmutils_setVmMemory.py
@@ -5,8 +5,4 @@ from nova.virt.hyperv import vmutils
 class TestSetVmMemory(unittest.TestCase):
 	def test_set_vm_memory(self):
 		mock = mock.MagicMock()
-        self._vmutils._lookup_vm_check = mock.MagicMock(
-            return_value=mock)
-        mock.path_.return_value = self._FAKE_VM_PATH
-        return mock
-
+       		
