cd6d99bd392238bf0a8a08e4ffb1889d8c47781c
Adrian Suhov <adynezu@gmail.com>
2014-07-22 18:20:45 +0300

    Update test_vmutils_setVmMemory.py

--- test_vmutils_setVmMemory.py
+++ test_vmutils_setVmMemory.py
@@ -3,6 +3,27 @@ import mock
 from nova.virt.hyperv import vmutils
 
 class TestSetVmMemory(unittest.TestCase):
-	def test_set_vm_memory(self):
-		mock = mock.MagicMock()
-       		
+	def _lookup_vm(self):
+        	mock_vm = mock.MagicMock()
+        	self._vmutils._lookup_vm_check = mock.MagicMock(
+        		return_value=mock_vm)
+	        mock_vm.path_.return_value = self._FAKE_VM_PATH
+		return mock_vm
+
+	def _test_set_vm_memory_dynamic(self, dynamic_memory_ratio):
+        	mock_vm = self._lookup_vm()
+        	mock_s = self._vmutils._conn.Msvm_VirtualSystemSettingData()[0]
+        	mock_s.SystemType = 3
+        	mock_vmsetting = mock.MagicMock()
+		mock_vmsetting.associators.return_value = [mock_s]
+        	self._vmutils._modify_virt_resource = mock.MagicMock()
+        	self._vmutils._set_vm_memory(mock_vm, mock_vmsetting,
+                                     self._FAKE_MEMORY_MB,
+                                     dynamic_memory_ratio)
+
+        	self._vmutils._modify_virt_resource.assert_called_with(
+            	mock_s, self._FAKE_VM_PATH)
+        if dynamic_memory_ratio > 1:
+            self.assertTrue(mock_s.DynamicMemoryEnabled)
+        else:
+            self.assertFalse(mock_s.DynamicMemoryEnabled)
